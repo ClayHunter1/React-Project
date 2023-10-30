307a26bf1c4aad07600a17f2da92e8fd0645845a
Clay Hunter <130602132+ClayHunter1@users.noreply.github.com>
2023-10-30 03:35:45 -0300

    Add nav bar JS file

---
+++ react-app/src/navBar.js
@@ -0,0 +1,14 @@
+export default function navBar() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
