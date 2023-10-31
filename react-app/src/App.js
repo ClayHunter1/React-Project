edfeeb1d97b4e575d45457d26aeedfd204f1f63a
Clay Hunter <130602132+ClayHunter1@users.noreply.github.com>
2023-10-31 07:17:26 -0300

    Add movie api data

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,34 @@
+import React, { useState, useEffect} from 'react';
 import Poster from "./Poster";
-import navBar from "./navBar";
+import navBar from "./NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MovieList from "./MovieList";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const getMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?s=star wars&apikey=c4be9593"
+
+    const response = await fetch(url);
+    const responsJson = await response.json();
+    setMovies(responsJson.Search);
+  };
+
+  useEffect(()=>{
+    getMovieRequest();
+  }, []);
+
   return (
     <>
       <Poster />
       <navBar />
+      <div className='container-fluid movie-app'>
+        <div className='row'>
+          <MovieList movies = {movies} />
+        </div>
+      </div>
     </>
   );
 }
